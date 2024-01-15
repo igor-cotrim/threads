926206c36ee610da3f73a31ad1d8d8a067e23b99
Igor Cotrim Santos <igorcotrim.dev@gmail.com>
2024-01-15 19:57:16 -0300

    feat: :sparkles: create a Topbar component

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -4,6 +4,12 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
+import {
+  Bottombar,
+  LeftSidebar,
+  RightSidebar,
+  Topbar,
+} from "@/components/shared";
 import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -26,11 +32,15 @@ export default function RootLayout({
     >
       <html lang="en">
         <body className={inter.className}>
+          <Topbar />
           <main className="flex flex-row">
+            <LeftSidebar />
             <section className="main-container">
               <div className="w-full max-w-4xl">{children}</div>
             </section>
+            <RightSidebar />
           </main>
+          <Bottombar />
         </body>
       </html>
     </ClerkProvider>
