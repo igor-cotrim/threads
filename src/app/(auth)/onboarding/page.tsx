ca71f70cb2d1969fd111063c83bd3766a78a96da
Igor Cotrim Santos <igorcotrim.dev@gmail.com>
2024-01-25 19:57:57 -0300

    feat: :sparkles: create a Community Page

--- src/app/(auth)/onboarding/page.tsx
+++ src/app/(auth)/onboarding/page.tsx
@@ -1,22 +1,25 @@
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
+import { fetchUser } from "@/lib/actions";
 import { AccountProfile } from "@/components/forms";
 
 async function Page() {
   const user = await currentUser();
-  const userInfo = {};
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect("/");
+
   const userData = {
-    id: user?.id!,
-    // objectId: userInfo?._id,
-    // username: userInfo?.username || user?.username,
-    // name: userInfo?.name || user?.firstName || "",
-    // bio: userInfo?.bio || "",
-    // image: userInfo?.image || user?.imageUrl,
-    objectId: "",
-    username: user?.username!,
-    name: user?.firstName! || "",
-    bio: "",
-    image: user?.imageUrl!,
+    id: user.id,
+    objectId: userInfo?._id,
+    username: userInfo ? userInfo?.username : user.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? "",
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user.imageUrl,
   };
 
   return (
--- src/components/cards/index.ts
+++ src/components/cards/index.ts
@@ -1,2 +1,3 @@
+export { default as CommunityCard } from "./CommunityCard";
 export { default as ThreadCard } from "./ThreadCard";
 export { default as UserCard } from "./UserCard";
