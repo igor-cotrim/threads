49292d129e674b3b4755b3f5c867ca911b1cf8a5
Igor Cotrim Santos <igorcotrim.dev@gmail.com>
2024-01-25 16:19:26 -0300

    feat: :sparkles: create Activity page

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -100,6 +100,31 @@ export async function fetchUsers({
   }
 }
 
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    const userThreads = await Thread.find({ author: userId });
+
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId },
+    }).populate({
+      path: "author",
+      model: User,
+      select: "_id name image",
+    });
+
+    return replies;
+  } catch (error: any) {
+    throw new Error(`Failed to get activity: ${error.message}`);
+  }
+}
+
 export async function updateUser({
   userId,
   username,
